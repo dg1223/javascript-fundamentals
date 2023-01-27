53fe6efd0a005094d7d228a85abbead868c0cfc1
dg1223 <alavi1223@hotmail.com>
2023-01-27 11:03:24 -0500

    continue

--- loop.js
+++ loop.js
@@ -14,5 +14,21 @@ for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
 // display all elements of an array
 var numbers = [45, 87, 89, 56, 32, 51, 25]
 for (var i=0; i<7; i++){
-    console.log(numbers[i])
+    // console.log(numbers[i])
+}
+
+// break and continue
+// for () {
+//     if (){
+//         break;
+//     }
+// }
+
+// continue -> skip and iterate
+for (var i=0; i<numbers.length; i++) {
+    var number = numbers[i];
+    if (number>50){
+        continue;
+    }
+    console.log(number)
 }
