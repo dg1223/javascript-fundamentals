73623ebedb9704ee8ce55965a0a8db6d2b5a2c40
dg1223 <alavi1223@hotmail.com>
2023-01-27 10:54:17 -0500

    for loop

--- loop.js
+++ loop.js
@@ -1,6 +1,12 @@
+// while loop
 var roastgiven = 0
 while (roastgiven < 7) {
-    console.log('Roast den, please')
-    console.log(roastgiven)
+    // console.log('Roast den, please')
+    // console.log(roastgiven)
     roastgiven++
+}
+
+// for loop
+for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
+    console.log('Roast den, please')
 }
