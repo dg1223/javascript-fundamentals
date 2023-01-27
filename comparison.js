06d47bfa38ba7dcc30e7a746e9dbf41c41031b79
dg1223 <alavi1223@hotmail.com>
2023-01-27 00:24:32 -0500

    array nested conditionals

--- comparison.js
+++ comparison.js
@@ -6,8 +6,32 @@ console.log( 5 != 6)
 // money1 > money2 || result1 > result || ...
 
 if (price > budget){
-    console.log("won't buy")
+    // console.log("won't buy")
 }
 else{
-    console.log("i'll buy something else")
+    // console.log("i'll buy something else")
+}
+
+// multiple conditionals
+if (){
+
+}
+else if (){
+
+}
+else{
+
+}
+
+// nested conditions
+if (){
+    if (){
+
+    }
+    else{
+        
+    }
+}
+else{
+
 }
