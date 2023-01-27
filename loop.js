0631c358daae4f8838f2caa4e85eb4bbaeb8bbbc
dg1223 <alavi1223@hotmail.com>
2023-01-27 10:57:11 -0500

    loop over array

--- loop.js
+++ loop.js
@@ -8,5 +8,11 @@ while (roastgiven < 7) {
 
 // for loop
 for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
-    console.log('Roast den, please')
+    // console.log('Roast den, please')
+}
+
+// display all elements of an array
+var numbers = [45, 87, 89, 56, 32, 51, 25]
+for (var i=0; i<7; i++){
+    console.log(numbers[i])
 }
