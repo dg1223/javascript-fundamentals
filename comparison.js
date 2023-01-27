6652ac7234cad2501415c3067e8769b5e9963699
dg1223 <alavi1223@hotmail.com>
2023-01-27 00:20:44 -0500

    array conditionals 2

--- comparison.js
+++ comparison.js
@@ -1,2 +1,13 @@
 console.log(5 == 6)
 console.log( 5 != 6)
+
+// multiple conditions
+// money1 > money2 && result1 > result && ...
+// money1 > money2 || result1 > result || ...
+
+if (price > budget){
+    console.log("won't buy")
+}
+else{
+    console.log("i'll buy something else")
+}
