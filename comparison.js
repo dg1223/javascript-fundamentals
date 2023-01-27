747e1a98cec85fd0fb8ad5a4e6b10746e7609171
dg1223 <alavi1223@hotmail.com>
2023-01-27 10:45:12 -0500

    while loop

--- comparison.js
+++ comparison.js
@@ -1,37 +1,39 @@
-console.log(5 == 6)
-console.log( 5 != 6)
+// console.log(5 == 6)
+// console.log( 5 != 6)
 
 // multiple conditions
 // money1 > money2 && result1 > result && ...
 // money1 > money2 || result1 > result || ...
 
-if (price > budget){
-    // console.log("won't buy")
-}
-else{
-    // console.log("i'll buy something else")
-}
+// if (price > budget){
+//     // console.log("won't buy")
+// }
+// else{
+//     // console.log("i'll buy something else")
+// }
 
-// multiple conditionals
-if (){
+// // multiple conditionals
+// if (){
 
-}
-else if (){
+// }
+// else if (){
 
-}
-else{
+// }
+// else{
 
-}
+// }
 
-// nested conditions
-if (){
-    if (){
+// // nested conditions
+// if (){
+//     if (){
 
-    }
-    else{
+//     }
+//     else{
         
-    }
-}
-else{
+//     }
+// }
+// else{
 
-}
+// }
+
+console.log(6 >= 6)
---
+++ loop.js
@@ -0,0 +1,6 @@
+var roastgiven = 0
+while (roastgiven < 7) {
+    console.log('Roast den, please')
+    console.log(roastgiven)
+    roastgiven++
+}
