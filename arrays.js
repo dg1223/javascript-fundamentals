768c9d8ba36a27d93bbf4b36a8bb29c4bb40bfd8
dg1223 <alavi1223@hotmail.com>
2023-01-26 23:54:11 -0500

    array index

--- arrays.js
+++ arrays.js
@@ -1,5 +1,15 @@
 var friendsAge = [11, 21, 45, 17, 14, 105, 6];
 var picnicFee = [5000, 2000, 4000, 150];
-var naikaj = ['mahi', 'opu', 'shabnoor', 'shabana']
+var naikaj = ['mahi', 'opu', 'shabnoor', 'shabana'];
 
-console.log(picnicFee.length)
+// console.log(picnicFee.length)
+
+// get element by index
+// console.log(picnicFee[0])
+
+picnicFee[3] = 44;
+// console.log(picnicFee[3])
+
+// Find index of an element
+var positionIndex = picnicFee.indexOf(48);
+console.log(positionIndex)
