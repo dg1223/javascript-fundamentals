8dbcc58688621506a21530c12dbc30da935dfd23
dg1223 <alavi1223@hotmail.com>
2023-02-01 20:01:00 -0500

    join

--- split-join.js
+++ split-join.js
@@ -11,7 +11,14 @@ const chars = lyrics.split('')
 // console.log(chars)
 
 const partial = lyrics.slice(5, 8)
-console.log(partial)
+// console.log(partial)
 
 const partial2 = lyrics.substring(5, 8)
-console.log(partial2)
+// console.log(partial2)
+
+const lines = ['Tumi bondhu kala pakhi ami jeno ki',
+                'Boshonto kale tomay bolte pari na', 
+                'Shada shada kala kala rong jomeche shada kala']
+
+const newSong = lines.join('. ')
+console.log(newSong)
