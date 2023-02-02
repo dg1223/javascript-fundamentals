4dc6c15d17c0717e1033b02b3d03f48e5ab4e115
dg1223 <alavi1223@hotmail.com>
2023-02-01 22:03:48 -0500

    reverse a string

--- reverse.js
+++ reverse.js
@@ -1,9 +1,13 @@
 function reverseString(text){
+    let reversed = ''
     for(let i=text.length-1; i>=0; i--){
         const element = text[i]
-        console.log(element)
+        reversed += element
+        // console.log(element)
     }
+    return reversed
 }
 
 const myString = 'I am a good boy'
 const reversed = reverseString(myString)
+console.log(reversed)
