e324341ed1409170c16fde7c0cc55f6a9df1aad8
dg1223 <alavi1223@hotmail.com>
2023-02-01 19:56:27 -0500

    split slice substring

---
+++ split-join.js
@@ -0,0 +1,17 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki. Boshonto kale tomay bolte pari na. Shada shada kala kala rong jomeche shada kala'
+
+const part = lyrics.split(' ')
+
+const sentences = lyrics.split('.')
+
+// console.log(part)
+// console.log(sentences)
+
+const chars = lyrics.split('')
+// console.log(chars)
+
+const partial = lyrics.slice(5, 8)
+console.log(partial)
+
+const partial2 = lyrics.substring(5, 8)
+console.log(partial2)
