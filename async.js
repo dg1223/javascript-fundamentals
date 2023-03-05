475f25f010ad8f82d53d40dc7d1bc35de778b53f
dg1223 <alavi1223@hotmail.com>
2023-03-05 05:37:58 -0500

    timeoutID and intervalID, x++ vs ++x

--- async.js
+++ async.js
@@ -5,9 +5,17 @@ console.log(2);
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
 // setTimeout(doSomething, 4000);
-setTimeout(() => {
+const timeoutId = setTimeout(() => {
   console.log("lazy logged");
 }, 4000);
+let num = 0;
+const intervalId = setInterval(() => {
+  // num++;
+  console.log(++num);
+  if (num === 10) {
+    clearInterval(intervalId);
+  }
+}, 1000);
 console.log(4);
 console.log(5);
 console.log(6);
