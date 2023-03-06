58cfbe90be5ccc0c18a922200b14519e745b5ed1
dg1223 <alavi1223@hotmail.com>
2023-03-06 01:50:30 -0500

    errors

---
+++ error.js
@@ -0,0 +1,24 @@
+// const num = 10;
+// num();
+
+// let x = 10;
+// x = y + 5;
+// console.log(x);
+
+// const obj = { name: "John" };
+// console.log(Array.isArray(Object.keys(obj)));
+// Object.keys(obj).forEach((prop) => console.log(prop));
+
+// const name = "John";
+// name.toUpperCase = () => {
+//   console.log("JoHN");
+// };
+// name.toUpperCase();
+// console.log(name.toUpperCase());
+
+// const arr = [1, 2, 3];
+// arr.slice(-2, 0);
+// const arr1 = arr.slice(-2, 0);
+// console.log(arr1);
+
+const greeting = "Hello, world!";
